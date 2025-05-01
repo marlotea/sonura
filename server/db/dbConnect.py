82d0ad86d7a5cc92d7a99525cf817b238bba979c
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-05-01 02:14:06 -0700

    unused connection

--- server/db/dbConnect.py
+++ server/db/dbConnect.py
@@ -7,14 +7,6 @@ from models.UserModels import Base
 
 load_dotenv()
 
-conn = psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-)
-
 DB_URL = os.getenv("DB_URL")
 
 engine = create_engine(DB_URL)
