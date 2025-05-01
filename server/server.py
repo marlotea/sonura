c82b6169812a66db284f34df41d5f9c8b0dd9bc2
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-05-01 03:41:06 -0700

    simple spotify setup

--- server/server.py
+++ server/server.py
@@ -7,7 +7,7 @@ import sqlalchemy
 # our modules
 from routes.routes import check_version
 from db.dbConnect import Session_Local, engine, create_tables
-from spotify_endpoints.spotify_endpoints import get_client, get_token
+from spotify.utils import *
 
 app = FastAPI()
 
@@ -28,6 +28,8 @@ def shutdown():
 async def root():
     return {"message": "Success"}
 
+
+# this shit will be migrated to a routes folder later on, just here for testing and easy access
 @app.get("/test")
 def get_spotify_data():
     id, secret = get_client()
@@ -41,6 +43,40 @@ def get_spotify_token():
     token = get_token()
     return {"token" : token}
 
+@app.get("spotify-auth")
+def get_spotify_auth():
+    token = get_token()
+    auth_token = get_auth_header(token)
+    return {"auth_token" : auth_token}
+
+
+@app.get("/artist")
+def search_artist(artist_name: Artist):
+    token = get_token()
+    try:
+        name = serach_for_artist(token, artist_name)
+        return {
+            "res" : name
+        }
+    except Exception as e:
+        return {
+            "Error" : e
+        }
+
+@app.get("/artist-id")
+def get_id_artist(artist_name : Artist):
+    token = get_token()
+    id = get_artist_id(token, artist_name)
+    return {"id" : id}
+
+@app.get("/artist-top-tracks")
+def get_top_tracks(artist_name : Artist):
+    token = get_token()
+    id = get_artist_id(token, artist_name)
+    top_tracks = get_songs_by_artist(token, id)
+    return {
+        "result" : top_tracks
+    }
 
 @app.get("/db")
 async def check_db():
